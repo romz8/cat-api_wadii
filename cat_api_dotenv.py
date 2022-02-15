7793c42f75875b6300173483ffc09e1484d3aa69
Romain JOBERT <romainjobert@Romains-MacBook-Air-M1.local>
2022-02-15 20:46:01 +0100

    forgot the space on a =

--- cat_api_dotenv.py
+++ cat_api_dotenv.py
@@ -9,7 +9,7 @@ API_KEY = os.getenv("API_KEY")
 url = 'https://api.thecatapi.com/v1/images/888'
 headers = {'x-api-key':API_KEY}
 
-r=  requests.get(url,headers=headers)
+r =  requests.get(url,headers=headers)
 image = r.json()
 
 print(image['url'])
