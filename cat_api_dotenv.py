731a98cf999bf23885d9a304b19f2ee2103aceb8
Romain JOBERT <romainjobert@Romains-MacBook-Air-M1.local>
2022-02-15 20:36:35 +0100

    all changes from previous PR

---
+++ cat_api_dotenv.py
@@ -0,0 +1,15 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+#get environment variable (in our case the api key)
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+url = 'https://api.thecatapi.com/v1/images/888'
+headers = {'x-api-key':API_KEY}
+
+r=  requests.get(url,headers=headers)
+image = r.json()
+
+print(image['url'])
